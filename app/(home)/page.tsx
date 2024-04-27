8d77e7d16d095a49250d51a1f5ef8b80a365f3bf
derekgygax <derekgygax@gmail.com>
2024-04-27 13:37:23 +0545

    hero on home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,13 +2,22 @@
 // layouts
 import { MainFullPage } from '@/shared/_layouts/mainFullPage/MainFullPage';
 
+// components
+import { Hero } from '../_components/hero/Hero';
+
+// content
+import content from '@/personal_content/homePage.json'
+
 // styles
 import styles from './page.module.scss';
 
 export default function HomePage() {
   return (
     <MainFullPage>
-      MAIN
+      <Hero
+        classNameBackground={styles.heroBackground}
+        content={content.hero}
+      />
     </MainFullPage>
   );
 }
