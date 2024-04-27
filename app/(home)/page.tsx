f447dbbd12db3b139d93b766c1c86e96b67a2de9
derekgygax <derekgygax@gmail.com>
2024-04-27 18:04:17 +0545

    made PastProjects.tsx for readability

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,47 +4,32 @@ import { MainFullPage } from '@/shared/_layouts/mainFullPage/MainFullPage';
 
 // components
 import { Hero } from '../_components/hero/Hero';
-import { PastProject } from '../_components/PastProject/PastProject';
 import { SectionHeader } from '../_components/sectionHeader/SectionHeader';
+import { FavoriteServices } from '../_components/favoriteServices/FavoriteServices';
+import { PastProjects } from '../_components/pastProjects/PastProjects';
 
 // shared components
 import { FavoriteReviews } from '@/shared/_components/favoriteReviews/FavoriteReviews';
 
 // content
-import homePageContent from '@/personal_content/homePage.json';
-import pastProjectsContent from '@/personal_content/pastProjects.json';
+import content from '@/personal_content/homePage.json';
 
 // styles
 import styles from './page.module.scss';
-import { CardsSection } from '../_layouts/cardSection/CardSection';
-import { ProjectType } from '@/personal_types/project';
-import { FavoriteServices } from '../_components/favoriteServices/FavoriteServices';
 
 export default function HomePage() {
   return (
     <MainFullPage>
       <Hero
         classNameBackground={styles.heroBackground}
-        content={homePageContent.hero}
+        content={content.hero}
       />
       <FavoriteServices />
-      <CardsSection
-        title={pastProjectsContent.title}
-        cards={
-          pastProjectsContent.projects.map((project: ProjectType, index: number) => {
-            return (
-              <PastProject
-                key={`pastProject_${index}`}
-                project={project}
-              />
-            )
-          })
-        }
-      />
+      <PastProjects />
       <FavoriteReviews
         sectionTitle={(
           <SectionHeader
-            title={homePageContent.favoriteReviews.title}
+            title={content.favoriteReviews.title}
           />
         )}
       />
---
+++ app/_components/pastProjects/PastProjects.tsx
@@ -0,0 +1,34 @@
+
+
+// types
+import { ProjectType } from '@/personal_types/project';
+
+// layouts
+import { CardsSection } from '@/app/_layouts/cardSection/CardSection';
+
+// components
+import { PastProject } from '../PastProject/PastProject';
+
+// content
+import content from '@/personal_content/pastProjects.json';
+
+// style
+import styles from './PastProjects.module.scss';
+
+export const PastProjects = () => {
+  return (
+    <CardsSection
+      title={content.title}
+      cards={
+        content.projects.map((project: ProjectType, index: number) => {
+          return (
+            <PastProject
+              key={`pastProject_${index}`}
+              project={project}
+            />
+          )
+        })
+      }
+    />
+  )
+}
