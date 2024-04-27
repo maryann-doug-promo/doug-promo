d826bcfb33a1208b06ef533ab61e90fd12ea0b74
derekgygax <derekgygax@gmail.com>
2024-04-27 20:22:54 +0545

    contactInfo finished

--- app/_components/contactInfo/ContactInfo.tsx
+++ app/_components/contactInfo/ContactInfo.tsx
@@ -1,3 +1,7 @@
+import classNames from 'classnames';
+
+// shared utils
+import { formatPhoneNumber } from '@/shared/lib/utils';
 
 // layouts
 
@@ -5,6 +9,7 @@
 
 // content
 import content from '@/personal_content/contactInfo.json';
+import personalInfo from '@/personal_content/personal_info.json';
 
 // styles
 import styles from './ContactInfo.module.scss';
@@ -15,6 +20,29 @@ interface ContactInfoProps {
 
 export const ContactInfo = ({ className }: ContactInfoProps) => {
   return (
-    <div className={className}>ContactInfo</div>
+    <div className={classNames(className, styles.contactInfo)}>
+      <h3 className={styles.h3}>{content.twentFourHours}</h3>
+      <div className={styles.instructions}>
+        {content.instructions.map((instruction: string, index: number) => {
+          return (
+            <p key={`contact_contactInfo_instruction_${index}`}>{instruction}</p>
+          )
+        })}
+      </div>
+      <h3 className={styles.h3}>{`${personalInfo.company_name}, ${personalInfo.slogan}`}</h3>
+      <div>
+        <h3 className={styles.h3}>{`${personalInfo.name.first} ${personalInfo.name.last}`}</h3>
+        <h3 className={styles.h3}>{personalInfo.email}</h3>
+        <h3 className={styles.h3}>{formatPhoneNumber(personalInfo.phone_number)}</h3>
+      </div>
+      <div>
+        <h5>{content.serviceAreas.title}</h5>
+        {content.serviceAreas.areas.map((area: string, index) => {
+          return (
+            <p key={`contact_contactInfo_serviceArea_${index}`}>{area}</p>
+          )
+        })}
+      </div>
+    </div>
   )
 }
